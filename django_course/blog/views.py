c47071fb4ec48c251a83225c67eb460aa98c5093
Stupel <marian.holly7117@gmail.com>
2024-11-11 21:59:35 +0100

    create routes

--- django_course/blog/views.py
+++ django_course/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("<h2>Welcome to Blog Home</h2>")
+
+def about(request):
+    return HttpResponse("<h2>About Blog</h2>")
